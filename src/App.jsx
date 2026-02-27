4707847cf3b153f7d61bc0546ad2c69354233b48
CodeWithVikas0810 <139254744+CodeWithVikas0810@users.noreply.github.com>
2026-02-27 10:45:01 +0530

    Integrate react-toastify and update App component
    Added ToastContainer for notifications and updated JSX structure.

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,23 @@ import Signup from "./Pages/Signup.jsx";
 import Login from "./Pages/Login.jsx";
 import TodoList from "./Pages/TodoList.jsx";
 import { Navigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"; // Import the CSS
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/TodoList" element={<TodoList />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/TodoList" element={<TodoList />} />
+        </Routes>
+      </BrowserRouter>
+
+      <ToastContainer />
+    </>
   );
 }
 
